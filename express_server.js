32a6d1dc9ae20590a18c0ee5d569682e7a97141e
javascriptsucks <evix1988@live.cn>
2023-01-23 12:25:32 -0600

    express-setup

---
+++ express_server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
+app.listen(PORT,() => {
+  console.log(`Now the server is on port: ${PORT}`);
+});
